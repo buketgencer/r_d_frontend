e012386ac36e5c4b10eb1429815962c012f3f9de
buketgencer <99204723+buketgencer@users.noreply.github.com>
2025-06-14 17:45:07 +0300

    feat: implement HomeLayout component and update routing

---
+++ src/layout/home-layout/layout.tsx
@@ -0,0 +1,63 @@
+import { Layout as AntLayout } from 'antd'
+import React from 'react'
+import styled from 'styled-components'
+import {
+	AppHeader,
+	ResizableSidebar,
+	useSidebarState,
+} from '@/layout/home-layout'
+
+const { Content } = AntLayout
+
+const StyledContent = styled(Content)`
+	padding: 24px;
+	margin: 0;
+	min-height: 280px;
+`
+
+interface LayoutProps {
+	defaultSidebarWidth?: number
+	minSidebarWidth?: number
+	maxSidebarWidth?: number
+	children?: React.ReactNode
+}
+
+export const HomeLayout: React.FC<LayoutProps> = ({
+	defaultSidebarWidth = 200,
+	minSidebarWidth = 120,
+
+	maxSidebarWidth = 600,
+  children,
+}) => {
+	const { collapsed, setCollapsed, siderWidth, isDragging, startResizing } =
+		useSidebarState({
+			defaultWidth: defaultSidebarWidth,
+			minWidth: minSidebarWidth,
+			maxWidth: maxSidebarWidth,
+		})
+
+	return (
+		<AntLayout>
+			<AntLayout
+				style={{ minHeight: '100vh' }}
+				hasSider={true}
+			>
+				<ResizableSidebar
+					collapsed={collapsed}
+					siderWidth={siderWidth}
+					isDragging={isDragging}
+					startResizing={startResizing}
+					collapsedWidth={0}
+					handleWidth={3}
+				/>
+				<AntLayout>
+					<AppHeader
+						collapsed={collapsed}
+						setCollapsed={setCollapsed}
+					/>
+					<StyledContent>{children}</StyledContent>
+				</AntLayout>
+			</AntLayout>
+		</AntLayout>
+	)
+}
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,11 @@
 import { createFileRoute } from '@tanstack/react-router'
+import { HomeLayout } from '@/layout/home-layout'
 import { Home } from '@/pages/home'
 
 export const Route = createFileRoute('/')({
-  component: Home,
+	component: () => (
+		<HomeLayout>
+			<Home />
+		</HomeLayout>
+	),
 })
-
